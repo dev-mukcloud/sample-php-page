4c172e4c22a237e44c62563a0320bb719c4b88a4
dev-mukcloud <32312166+dev-mukcloud@users.noreply.github.com>
2017-09-27 00:01:34 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo "<!DOCTYPE html>";
+
+echo "<html>";
+echo "<head>";
+echo "<title>27 September 2017</title>";
+echo "<meta charset="utf-8" />";
+
+echo "</head>";
+echo "<body>";
+echo "<H1> Hello world! </H1>";
+echo "<p>";
+while ($i< 1000000)
+{
+  echo "i".$i.";";
+}
+echo "</p>";
+  
+echo "</body>";
+echo "</html>";
+?>
